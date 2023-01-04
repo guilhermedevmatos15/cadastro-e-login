50e5f1b46909badee98f68687571fcd38e48d507
Guilherme Magno Matos <115946540+guilhermedevmatos15@users.noreply.github.com>
2023-01-04 16:52:46 -0300

    JavaScript feito

--- register-page/script.js
+++ register-page/script.js
@@ -4,7 +4,8 @@ const passwordInput = document.querySelector('input#password');
 const passwordConfirmationInput = document.querySelector('input#password-confirmation');
 const button = document.querySelector('button#create-an-account');
 const buttonLogin = document.querySelector('button#faça-login');
-const main = document.querySelector('main');
+
+// OBS: aside é uma div semantica do HTML, então pense assim: 'aside é uma div'.
 
 const validations = {
    user() {
@@ -14,16 +15,13 @@ const validations = {
          asideUser.classList.add('error');
          asideUser.classList.remove('success');
          message.innerHTML='Nome de usuário muito pequeno';
-         return false;
       } else if (userInput.value.length > 20) {
          asideUser.classList.add('error');
          asideUser.classList.remove('success');
          message.innerHTML='Nome de usuário muito grande';
-         return false;
       } else {
          asideUser.classList.remove('error');
          asideUser.classList.add('success');
-         return true;
       }
    },
    email() {
@@ -32,12 +30,10 @@ const validations = {
       if (emailInput.value.includes('@') === true) {
          asideEmail.classList.remove('error');
          asideEmail.classList.add('success');
-         return true;
       } else if (emailInput.value.includes('@') === false) {
          asideEmail.classList.add('error');
          asideEmail.classList.remove('success');
-         message.innerHTML='Insira um email válido'
-         return false;
+         message.innerHTML='Insira um email válido';
       }
    },
    password() {
@@ -47,11 +43,9 @@ const validations = {
          asidePassword.classList.add('error');
          asidePassword.classList.remove('success');
          message.innerHTML='Senha muito pequena';
-         return false;
       } else {
          asidePassword.classList.remove('error');
-         asidePassword.classList.add('success');         
-         return true;
+         asidePassword.classList.add('success');
       }
    },
    passwordConfirmation() {
@@ -61,11 +55,9 @@ const validations = {
          asidePasswordConfirmation.classList.add('error');
          asidePasswordConfirmation.classList.remove('success');
          message.innerHTML='Senhas não estão iguais';
-         return false;
       } else {
          asidePasswordConfirmation.classList.remove('error');
-         asidePasswordConfirmation.classList.add('success');         
-         return true;
+         asidePasswordConfirmation.classList.add('success');
       }
    },
 }
@@ -73,7 +65,9 @@ const validations = {
 userInput.addEventListener('keyup', validations.user);
 emailInput.addEventListener('keyup', validations.email);
 passwordInput.addEventListener('keyup', validations.password);
+passwordInput.addEventListener('keyup', validations.passwordConfirmation);
 passwordConfirmationInput.addEventListener('keyup', validations.passwordConfirmation);
+
 button.addEventListener('click', () => {
    const allAsides = [...document.querySelectorAll('aside')];
    const inputsValidateds = allAsides.filter((aside) => {
@@ -97,9 +91,11 @@ button.addEventListener('click', () => {
          span.innerHTML='';
       }
       // Acessar página de conta criada!
-      window.location.href = "";
+      // window.location.href = "";
    }
 });
+
 buttonLogin.addEventListener('click', () => {
-   window.open('login-page/login.html');
+   window.location.assign("http://pt.stackoverflow.com");
+   // window.open('login-page/login.html');
 });
